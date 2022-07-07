18a2270a1ba09a876b2a741c010097b4882557e3
David Bijok <106277069+Liioan@users.noreply.github.com>
2022-07-07 12:38:21 +0200

    Add files via upload
    fixed starting button position - it is now random, added humiliating texts after too many attempts, you can now actually click 'no' button

--- idiot_test/script.js
+++ idiot_test/script.js
@@ -2,24 +2,46 @@ const yesButton = document.querySelector('.yes');
 const noButton = document.querySelector('.no');
 const idiotButton = document.querySelector('.idiot');
 const wrapper = document.querySelector('.wrapper');
+const spans = document.querySelectorAll('span');
 
-noButton.addEventListener('click', () => {
+let xPosition = Math.round(Math.random() * 90);
+let yPosition = Math.round(Math.random() * 90);
+idiotButton.style.left = `+${xPosition}%`;
+idiotButton.style.top = `+${yPosition}%`;
+
+noButton.addEventListener('mouseover', () => {
     // console.log('clicked no');
     noButton.classList.add('hidden')
     idiotButton.classList.remove('hidden');
 });
 
+yesButton.addEventListener('click', () => {
+    // console.log('clicked yes');
+    wrapper.innerHTML = `<div><span class="ez">I knew it ;)</span></div>`
+    idiotButton.classList.add('hidden');
+});
+
+let hoverCounter = 0;
+
 idiotButton.addEventListener('mouseover', () => {
     // console.log('idiot button clicked');
-    let xPosition = Math.round(Math.random() * 90);
-    let yPosition = Math.round(Math.random() * 90);
+    hoverCounter++;
+    if(hoverCounter === 5){
+        spans[0].classList.remove('hidden');
+    } else if(hoverCounter === 10){
+        spans[1].classList.remove('hidden');
+    } else if(hoverCounter === 15){
+        spans[2].classList.remove('hidden');
+    }
+
+    xPosition = Math.round(Math.random() * 90);
+    yPosition = Math.round(Math.random() * 90);
     idiotButton.style.left = `+${xPosition}%`;
     idiotButton.style.top = `+${yPosition}%`; 
     // console.log(xPosition, yPosition);   
 });
 
-yesButton.addEventListener('click', () => {
-    // console.log('clicked yes');
-    wrapper.innerHTML = `<div class.question-wrapper><span class="ez">I knew it ;)</span></div>`
+idiotButton.addEventListener('click', () => {
+    wrapper.innerHTML = `<div><span class="ez">Alright I belive you</span></div>`
     idiotButton.classList.add('hidden');
 });
